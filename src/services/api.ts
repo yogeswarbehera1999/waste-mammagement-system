07774fcc0588c5e83d609a376bccf56b79771203
YOGESWAR BEHERA <yogeswar.personal@gmail.com>
2025-06-23 17:11:58 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+const API_BASE_URL = 'https://wastemammagementsystembackend.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
