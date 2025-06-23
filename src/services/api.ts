3d52207d848fa3cb7e14d07de821110d9d487dd5
briskode <yogeswar.personal@gmail.com>
2025-06-23 17:45:39 +0530

    made changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5001/api';
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+if (!API_BASE_URL) {
+  throw new Error('VITE_API_URL is not defined in the environment variables');
+}
 
 const api = axios.create({
   baseURL: API_BASE_URL,
